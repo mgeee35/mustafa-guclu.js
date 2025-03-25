18d46c793585197ac49f8bc185761c6c5d6c991c
mgeee35 <mgeee35@proton.me>
2025-03-25 03:09:54 +0300

    constant variables defined

---
+++  mustafa-guclu.js
@@ -0,0 +1,15 @@
+/**
+ * @author Mustafa Guclu
+ */
+
+(function foo() {
+    const API_URL = "https://gist.githubusercontent.com/sevindi/5765c5812bbc8238a38b3cf52f233651/raw/56261d81af8561bf0a7cf692fe572f9e1e91f372/products.json";
+    const STORAGE_KEY = "recommended_products";
+    const FAVORITES_KEY = "favorite_products";
+    
+    function init() {
+
+    }
+    
+    init();
+})();
